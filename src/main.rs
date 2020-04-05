d89a9a0febc283d1e42ae3c2379c681c561ba90b
sunli <scott_s829@163.com>
2020-04-05 18:59:27 +0800

    a

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,7 @@
-// use jemallocator::Jemalloc;
-//
-// #[cfg(not(target_env = "msvc"))]
-// #[global_allocator]
-// static GLOBAL: Jemalloc = Jemalloc;
-// use mimallocator::Mimalloc;
-
 mod async_graphql_benchmark;
 mod juniper_benchmark;
 
-#[tokio::main]
+#[async_std::main]
 async fn main() {
     juniper_benchmark::run().await;
     async_graphql_benchmark::run().await;
