b5d78e363e1ef95cea1b5af20ddaf2c7a472fa1f
Sunli <scott_s829@163.com>
2021-11-20 10:40:29 +0800

    update

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 mod async_graphql_benchmark;
 mod juniper_benchmark;
 
-#[async_std::main]
+#[tokio::main]
 async fn main() {
     juniper_benchmark::run().await;
     async_graphql_benchmark::run().await;
